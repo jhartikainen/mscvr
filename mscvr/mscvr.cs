64ff19bb5b5440b213ce4a5ab35fb4a241a52e9f
Jani Hartikainen <jani.hartikainen@gmail.com>
2018-07-24 11:42:53 +0300

    Add project files.

---
+++ mscvr/mscvr.cs
@@ -0,0 +1,25 @@
+﻿using MSCLoader;
+using UnityEngine;
+
+namespace mscvr {
+    public class mscvr : Mod {
+        public override string ID => "mscvr";
+        public override string Name => "mscvr";
+        public override string Author => "Your Username";
+        public override string Version => "1.0";
+
+        //Set this to true if you will be load custom assets from Assets folder.
+        //This will create subfolder in Assets folder for your mod.
+        public override bool UseAssetsFolder => false;
+
+        //Called when mod is loading
+        public override void OnLoad() {
+
+        }
+
+        // Update is called once per frame
+        public override void Update() {
+
+        }
+    }
+}
